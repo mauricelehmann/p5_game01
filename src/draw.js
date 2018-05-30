84de1b01c1f88b6158277f5f518fccfe33600442
mauricelehmann <m.lehmann94@gmail.com>
2018-05-30 12:59:16 +0200

    comment

--- src/draw.js
+++ src/draw.js
@@ -1,6 +1,9 @@
+//Globals objects
 var mainCircle = new Circle(10,10,50,0,255,0) ;
 var dotArray = [] ;
 
+
+//Setup , run once
 function setup() {
 	//Canvas setup
 	var canevas = createCanvas(windowWidth - 20 ,windowHeight -20 );
@@ -8,12 +11,16 @@ function setup() {
 	fillDotArray()
 }
 
+//Draw , run constantly
 function draw() {
 
 	//Reset the background
 	background(255);
-	//Create an ellipse todo : make 
+	//Create an ellipse todo : make
 	mainCircle.showCircle() ;
+	//set dot(s)
+	printDotArray() ;
+
 
 	//Listening inputs for movements
 	movementListener();
@@ -21,8 +28,6 @@ function draw() {
 	actionListener();
 	//Listening collision
 	collisionListener();
-	//set dot(s)
-	printDotArray() ;
 	//Check if the game is over and reset some parameters
 	endGameListener();
 }
